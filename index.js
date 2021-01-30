80ab355a52652eb52ade787068b04ca33fb3a97a
Ahmed Salama <eng.ahd746@gmail.com>
2021-01-30 14:27:38 -0500

    server

--- index.js
+++ index.js
@@ -107,13 +107,11 @@ app.listen(process.env.PORT || 5555, (err) => {
   }
 });
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+app.use(express.static("client/build"));
 
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
-  );
-}
+app.get("*", (req, res) =>
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+);
 
 // last resorts
 process.on("uncaughtException", (err) => {
