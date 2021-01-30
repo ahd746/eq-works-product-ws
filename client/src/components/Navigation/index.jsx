5f2c8c802a076ca505c206855020b0dbdb441aa2
Ahmed Salama <eng.ahd746@gmail.com>
2021-01-30 15:34:09 -0500

    client

--- client/src/components/Navigation/index.jsx
+++ client/src/components/Navigation/index.jsx
@@ -6,25 +6,27 @@ const Navigation = () => {
     <div className="mb-4">
       <Navbar bg="dark" variant="dark" sticky="top">
         <Navbar.Brand>EQ Work</Navbar.Brand>
-        <Nav className="mr-auto">
-          <Nav.Link as={Link} to="/">
-            Home
-          </Nav.Link>
-          <Nav.Link as={Link} to="/locations">
-            Locations
-          </Nav.Link>
-          <Nav.Link as={Link} to="/map">
-            Map
-          </Nav.Link>
-          <NavDropdown title="Charts">
-            <NavDropdown.Item as={Link} to="/hourly">
-              Hourly
-            </NavDropdown.Item>
-            <NavDropdown.Item as={Link} to="/daily">
-              Daily
-            </NavDropdown.Item>
-          </NavDropdown>
-        </Nav>
+        <Navbar.Collapse>
+          <Nav className="mr-auto">
+            <Nav.Link as={Link} to="/">
+              Home
+            </Nav.Link>
+            <Nav.Link as={Link} to="/locations">
+              Locations
+            </Nav.Link>
+            <Nav.Link as={Link} to="/map">
+              Map
+            </Nav.Link>
+            <NavDropdown title="Charts">
+              <NavDropdown.Item as={Link} to="/hourly">
+                Hourly
+              </NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/daily">
+                Daily
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
       </Navbar>
     </div>
   );
